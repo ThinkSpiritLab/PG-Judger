68963905ec86927dafebfa261f68ae4aac4f0f8d
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2024-06-10 20:50:30 +0800

    WIP: add sqlite3

--- src/modules/sql/sql.service.ts
+++ src/modules/sql/sql.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import Database from 'better-sqlite3';
 
 @Injectable()
-export class SqlService {}
+export class SqlService {
+  constructor() {
+    // const db = new Database('foobar.db', {
+    //   verbose: console.log,
+    // });
+    // db.pragma('journal_mode = WAL');
+    
+  }
+}
